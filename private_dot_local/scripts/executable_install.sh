49190dd9d784c7000421b33a181f69c4e0e13b2d
Daniel Yrovas <dan@danielyrovas.com>
2022-08-03 10:12:50 +1000

    install script

---
+++ private_dot_local/scripts/executable_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+help_menu() {
+  echo "Install script to automate installation of dependencies and configs"
+  echo "Options:"
+}
+
+configure() {
+  DISTRO="none"
+  grep "Arch Linux" /etc/os-release &> /dev/null && DISTRO="archlinux"
+  grep "Tumbleweed" /etc/os-release &> /dev/null && DISTRO="tumbleweed"
+
+}
+
+bootstrap() {
+  case $DISTRO in
+    "archlinux") pacstrap ;;
+    "tumbleweed") tumbledry ;;
+    *) echo "Linux distro not recognised" ;;
+  esac
+}
+
+pacstrap() {
+  echo "Installing packages for Arch Linux"
+
+  # paru-bin https://aur.archlinux.org/paru-bin.git, makepkg -si
+  # flatpak, xdg portals
+  # Rust, rust nightly for EWW
+}
+
+tumbledry() {
+  echo "Installing packages for Arch Linux"
+}
+
+# help_menu
+configure
+bootstrap
+
